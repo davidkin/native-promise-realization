b15badbf835e2ca9ef1cd5b0d130c2c9ae3c274f
David <davidfloyd322@gmail.com>
2019-01-14 14:10:53 +0200

    t/implementation, race method was added

--- src/promise.js
+++ src/promise.js
@@ -2,8 +2,27 @@ const RESOLVED = 'RESOLVED';
 const PENDING = 'PENDING';
 const REJECTED = 'REJECTED';
 
-const isResolved = () => this.state = RESOLVED;
-const isRejected = () => this.state = REJECTED;
+const isResolved = () => {
+  this.state = RESOLVED;
+};
+
+const isRejected = () => {
+  this.state = REJECTED;
+};
+
+// const isIterable = function(subject) {
+//   return subject !== null && typeof subject[Symbol.iterator] === 'function';
+// };
+
+
+// const validateIterable = subject => {
+//   if (isIterable(subject)) {
+//     return;
+//   }
+
+//   throw new TypeError(`Cannot read property 'Symbol(Symbol.iterator)' of ${Object.prototype.toString.call(subject)}.`);
+// };
+
 
 class OwnPromise {
   constructor(executer) {
@@ -13,24 +32,6 @@ class OwnPromise {
     if (typeof executer !== 'function') {
       throw new TypeError('Executer is not function');
     }
-
-    const resolve = data => {
-      // console.log('in resolve')
-      if (this.state !== PENDING) {
-        return;
-      }
-
-      if (this.__isThenable(data) && data.state === PENDING) {
-        // console.log('data.then')
-        data.then(v => resolve(v), v => reject(v));
-      } else {
-        this.state = this.__isThenable(data) ? data.state : RESOLVED;
-        this.value = this.__isThenable(data) ? data.value : data;
-        // console.log('in resolve', this.state, this.value)
-
-        this.__callHandlers();
-      }
-    };
 
     const reject = error => {
       if (this.state !== PENDING) {
@@ -45,6 +46,22 @@ class OwnPromise {
       });
     };
 
+
+    const resolve = data => {
+      if (this.state !== PENDING) {
+        return;
+      }
+
+      if (this.__isThenable(data) && data.state === PENDING) {
+        data.then(v => resolve(v), v => reject(v));
+      } else {
+        this.state = this.__isThenable(data) ? data.state : RESOLVED;
+        this.value = this.__isThenable(data) ? data.value : data;
+
+        this.__callHandlers();
+      }
+    };
+
     try {
       executer(resolve, reject);
     } catch (error) {
@@ -55,7 +72,6 @@ class OwnPromise {
   __isThenable(obj) {
     return obj && obj.then;
   }
-
   __callHandlers() {
     const run = () => {
       this.callbacks.forEach((callback, i) => {
@@ -134,7 +150,29 @@ class OwnPromise {
       reject(error);
     });
   }
+
+  static race(iterable) {
+    if (typeof this !== 'function') {
+      throw new TypeError('this is not instance from OwnPromise');
+    }
+
+    return new OwnPromise((resolve, reject) => {
+      iterable.forEach(value => value.then(resolve, reject));
+    });
+  }
 }
+
+
+// const p = new OwnPromise((res, rej) => {
+//   res(1);
+// });
+
+// const a = p.then(v => console.log(1)).then(v => console.log(2));
+
+// const b = p.then(v => console.log(3));
+
+// console.log('---', a);
+// console.log('---', b);
 
 
 module.exports = OwnPromise;
